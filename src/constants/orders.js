bea7d319e79dd7a989d6838fa9a18a1b4c7d011d
shivalilapare00 <shivalilapare00@gmail.com>
2024-03-14 19:39:03 +0530

    Initial commit

---
+++ src/constants/orders.js
@@ -0,0 +1,30 @@
+export const allOrders = [
+  {
+    id: 1,
+    orderID: "ORD001",
+    customerName: "Pooja",
+    orderDate: "2024-02-04",
+    status: "Processing",
+  },
+  {
+    id: 2,
+    orderID: "ORD002",
+    customerName: "Amruta",
+    orderDate: "2024-04-05",
+    status: "Processing",
+  },
+  {
+    id: 3,
+    orderID: "ORD003",
+    customerName: "Shivalila",
+    orderDate: "2023-11-25",
+    status: "Processing",
+  },
+  {
+    id: 4,
+    orderID: "ORD004",
+    customerName: "Neha",
+    orderDate: "2024-03-10",
+    status: "Processing",
+  },
+];
---
+++ src/constants/products.js
@@ -0,0 +1,37 @@
+export const allProducts = [
+  {
+    id: 1,
+    name: "VLCC",
+    category: "Skincare",
+    price: 200,
+    stock: 50,
+  },
+  {
+    id: 2,
+    name: "Smytten",
+    category: "Haircare",
+    price:800,
+    stock: 30,
+  },
+  {
+    id: 3,
+    name: "Plum",
+    category: "Skincare",
+    price: 900,
+    stock: 70,
+  },
+  {
+    id: 4,
+    name: "juice Cosmetics",
+    category: "Makeup",
+    price: 1000,
+    stock: 25,
+  },
+  {
+    id: 5,
+    name: "Soulflower",
+    category: "Haircare",
+    price: 445,
+    stock: 25,
+  },
+];
